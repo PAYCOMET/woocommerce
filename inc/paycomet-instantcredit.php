bf0e6677f545a8a033e12698a6e0d22566ee70a1
Jose Ramón García <jrgarcia@paycomet.com>
2021-05-24 16:25:56 +0200

    Se añade el simulador de Instant Credit

--- inc/paycomet-instantcredit.php
+++ inc/paycomet-instantcredit.php
@@ -1,14 +1,14 @@
 <?php
 
 if (isset(get_option('woocommerce_paycomet_instantcredit_settings')['calculator_position']) && isset(get_option('woocommerce_paycomet_instantcredit_settings')['priority_simulator'])) {
-    add_action(get_option('woocommerce_paycomet_instantcredit_settings')['calculator_position'], 'woocommerce_paycomet_instantcredit_show_instantcredit_calculator', get_option('woocommerce_paycomet_instantcredit_settings')['priority_simulator']); 
+    add_action(get_option('woocommerce_paycomet_instantcredit_settings')['calculator_position'], 'woocommerce_paycomet_instantcredit_show_instantcredit_calculator', get_option('woocommerce_paycomet_instantcredit_settings')['priority_simulator']);
 }
 
 add_action( 'wp_enqueue_scripts', 'woocommerce_paycomet_instantcredit_enqueue_scripts' );
 
 function woocommerce_paycomet_instantcredit_enqueue_scripts() {
     if (!is_product() && !is_checkout() &&!is_cart() ) return;
-    $ic_simulator = 'https://instantcredit.net/simulator/test/ic-simulator.js';
+    $ic_simulator = 'https://instantcredit.net/simulator/ic-simulator.js';      // Prod
     wp_enqueue_script( 'woocommerce_paycomet_instantcredit-ic-calculator', $ic_simulator, array(), '1.0.3', true );
 }
 
@@ -17,17 +17,17 @@ function woocommerce_paycomet_instantcredit_add_id_to_script( $tag, $handle, $sr
     if ( 'woocommerce_paycomet_instantcredit-ic-calculator' === $handle ) {
         $tag = '<script type="text/javascript" src="' . esc_url( $src ) . '" id="icCalculator" charset="UTF-8"></script>';
     }
- 
+
     return $tag;
 }
 
 function woocommerce_paycomet_instantcredit_show_instantcredit_calculator(){
-    
+
     if (!is_product() && !is_checkout() && !is_cart() ) return;
     global $product;
     $id = $product->get_id();
     $cfg = get_option('woocommerce_paycomet_instantcredit_settings');
- 
+
     // 1. Variable products
     if( $product->is_type('variable') ){
         // Searching for the default variation
@@ -90,7 +90,7 @@ class Paycomet_Instantcredit extends Paycomet_APM
         $this->id = 'paycomet_instantcredit';
         $this->icon = PAYTPV_PLUGIN_URL . 'images/apms/instantcredit.svg';
         $this->has_fields = false;
-        $this->method_title = 'PAYCOMET - InstantCredit';
+        $this->method_title = 'PAYCOMET - Instant Credit';
         $this->method_description = sprintf( __( 'PAYCOMET general data must be configured <a href="%s">here</a>.', 'wc_paytpv' ), admin_url( 'admin.php?page=wc-settings&tab=checkout&section=paytpv' ) );
         $this->methodId = 33;
         $this->title = __('Pay with Instant Credit', 'wc_paytpv' );
@@ -115,18 +115,20 @@ class Paycomet_Instantcredit extends Paycomet_APM
         global $woocommerce;
         if (defined('DOING_AJAX') && DOING_AJAX) {
             $result = wpautop( wptexturize( $this->description ) );
+            print $result;
+
             $cart_total = $woocommerce->cart->get_total('');
             $cfg = get_option('woocommerce_paycomet_instantcredit_settings');
-
             if ($cfg) {
-                if ( ( $cart_total > $cfg['maxCost'] ) || ( $cart_total < $cfg['minCost'] ) ) return;
-    
-                $result.='<div class="ic-configuration" style="display:none;">'.$cfg['simulatorHash'].'</div>';
+
+                if ( !isset($cfg['maxCost']) || ( $cart_total > $cfg['maxCost'] ) || !isset($cfg['minCost']) || ( $cart_total < $cfg['minCost'] ) ) return;
+
+                $result ='<div class="ic-configuration" style="display:none;">'.$cfg['simulatorHash'].'</div>';
                 $result.='<div class="ic-simulator" amount="'.$cart_total.'"></div>';
                 $result.='<script>console.log("dentro");icSimulator = new ICSimulator();icSimulator.loadScript("https://code.jquery.com/jquery-1.12.0.min.js", function(){icSimulator.initialize();});;console.log(icSimulator);</script>';
-    
+
             }
-            print $result; 
+            print $result;
         }
     }
 
@@ -157,9 +159,9 @@ class Paycomet_Instantcredit extends Paycomet_APM
                         'woocommerce_before_add_to_cart_quantity' => __('before_add_to_cart_quantity', 'wc_paytpv' ),
                         'woocommerce_after_add_to_cart_quantity' => __('after_add_to_cart_quantity', 'wc_paytpv' ),
                         'woocommerce_after_single_product_summary' => __('after_single_product_summary', 'wc_paytpv' ),
-                        'woocommerce_after_add_to_cart_button' => __('after_add_to_cart_button', 'wc_paytpv' ),	
+                        'woocommerce_after_add_to_cart_button' => __('after_add_to_cart_button', 'wc_paytpv' ),
                     ),
-                    'description' => __( 'Selecto position to show Instant Credit calculator','wc_paytpv'),
+                    'description' => __( 'Select position to show Instant Credit calculator','wc_paytpv'),
                     'desc_tip'    => true,
                     'default'     => 'woocommerce_before_single_product'
             ),
@@ -177,23 +179,23 @@ class Paycomet_Instantcredit extends Paycomet_APM
                         '8' => __('8', 'wc_paytpv' ),
                         '9' => __('9', 'wc_paytpv' ),
                         '10' => __('10', 'wc_paytpv' ),
-                        '11' => __('11', 'wc_paytpv' ),	
-                        '12' => __('12', 'wc_paytpv' ),	
-                        '13' => __('13', 'wc_paytpv' ),	
-                        '14' => __('14', 'wc_paytpv' ),	
-                        '15' => __('15', 'wc_paytpv' ),	
-                        '16' => __('16', 'wc_paytpv' ),	
-                        '17' => __('17', 'wc_paytpv' ),	
-                        '18' => __('18', 'wc_paytpv' ),	
-                        '19' => __('19', 'wc_paytpv' ),	
-                        '20' => __('20', 'wc_paytpv' ),	
+                        '11' => __('11', 'wc_paytpv' ),
+                        '12' => __('12', 'wc_paytpv' ),
+                        '13' => __('13', 'wc_paytpv' ),
+                        '14' => __('14', 'wc_paytpv' ),
+                        '15' => __('15', 'wc_paytpv' ),
+                        '16' => __('16', 'wc_paytpv' ),
+                        '17' => __('17', 'wc_paytpv' ),
+                        '18' => __('18', 'wc_paytpv' ),
+                        '19' => __('19', 'wc_paytpv' ),
+                        '20' => __('20', 'wc_paytpv' ),
                     ),
-                    'description' => __( 'Selecto position to show Instant Credit calculator','wc_paytpv'),
+                    'description' => __( 'Select priority of Instant Credit calculator','wc_paytpv'),
                     'desc_tip'    => true,
                     'default'     => '10'
             ),
             'minCost' => array(
-                'title'       => __( 'Min. product price', 'wc_paytpv' ),
+                'title'       => __( 'Min. cart price', 'wc_paytpv' ),
                 'type'        => 'text',
                 'description' => __( 'Minimun purchase value.', 'wc_paytpv' ),
                 'default'     => '28',
