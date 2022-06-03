0bfb001c85ff5d87afebd9b76875317558fb0fef
Jose Ramón García <jrgarcia@paycomet.com>
2022-06-03 09:51:34 +0200

    HESK-5474 Control precio unitario

--- gateway-paytpv.php
+++ gateway-paytpv.php
@@ -5,7 +5,7 @@
  * Description: The PAYCOMET payment gateway for WooCommerce
  * Author: PAYCOMET
  * Author URI: https://www.paycomet.com
- * Version: 5.19
+ * Version: 5.20
  * Tested up to: 5.9
  * WC tested up to: 6.4.1
  * Text Domain: wc_paytpv
@@ -13,7 +13,7 @@
  */
 
 
-define( 'PAYTPV_VERSION', '5.19' );
+define( 'PAYTPV_VERSION', '5.20' );
 
 define( 'PAYTPV_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'PAYTPV_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
