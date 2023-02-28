4c0fbaa3c0c0672b61edd42158b91449cdef16d1
Jose Ramón García <jrgarcia@paycomet.com>
2023-02-28 15:31:40 +0100

    Depuración de código

--- inc/thankyou.php
+++ inc/thankyou.php
@@ -1,28 +1,33 @@
 <?php
 
+
 add_action( 'woocommerce_order_details_after_order_table', 'hwn_add_thankyou_custom_text');
 add_action( 'woocommerce_email_after_order_table', 'hwn_add_thankyou_custom_text');
 add_action( 'woocommerce_order_details_before_order_table', 'hwn_add_thankyou_mbway_confirm');
 
 
 function hwn_add_thankyou_custom_text($order_id) {
-    if($order_id->payment_method == "paycomet_multibanco"){
-        if ($order_id->meta_data[1]->value->entityNumber != '' && $order_id->meta_data[1]->value->referenceNumber != '') { 
-            ?>
-                <section id="order-paycomet" class="box" style="display: {$display}">
-                    <p>Información de pago de multibanco:</p>
-                    <ul style="list-style-type: none;">
-                        <li><strong>Entidad: </strong> <?php echo $order_id->meta_data[1]->value->entityNumber;?></li>
-                        <li><strong>Referencia: </strong> <?php echo $order_id->meta_data[1]->value->referenceNumber;?></li>
-                    </ul>
-                </section>
-            <?php 
-        }    
+
+    if($order_id->get_payment_method() == "paycomet_multibanco"){
+        if ($order_id->get_meta("PayTPV_methodData")) {
+            $datos = $order_id->get_meta("PayTPV_methodData");
+            if ($datos->entityNumber != '' && $datos->referenceNumber != '') {
+                ?>
+                    <section id="order-paycomet" class="box">
+                        <p>Información de pago de multibanco:</p>
+                        <ul style="list-style-type: none;">
+                            <li><strong>Entidad: </strong> <?php echo $datos->entityNumber;?></li>
+                            <li><strong>Referencia: </strong> <?php echo $datos->referenceNumber;?></li>
+                        </ul>
+                    </section>
+                <?php 
+            }
+        }
     }
 }
 
 function hwn_add_thankyou_mbway_confirm($order_id) {
-    if ($order_id->payment_method == "paycomet_mbway"){
+    if ($order_id->get_payment_method() == "paycomet_mbway"){
         ?>
             <p><strong><?php echo sprintf(__( 'You must confirm the purchase on MB WAY, through the notice or in the activity area', 'wc_paytpv'));?></strong></p>  
         <?php
